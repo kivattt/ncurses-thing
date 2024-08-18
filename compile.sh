c8bbee05047ba7305186b09666071ff7cb0f0bb6
kivattt <kivattt@gmail.com>
2024-08-18 04:52:16 +0200

    Fix building on Astral

--- compile.sh
+++ compile.sh
@@ -1,1 +1,6 @@
-g++ -std=c++23 -O2 main.cpp -o main -lncurses -lcurses
+unameStr=$(uname)
+if [[ unameStr == 'Astral' ]]; then
+	g++ -std=c++23 -O2 main.cpp -o main -lncurses -lcurses -ltinfow
+else
+	g++ -std=c++23 -O2 main.cpp -o main -lncurses -lcurses
+fi
