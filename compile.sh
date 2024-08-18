bd7e4739a69113cf1198bb65b5cd96b5f20d3a88
kivattt <kivattt@gmail.com>
2024-08-18 04:53:26 +0200

    Forgot a dollar sign

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 unameStr=$(uname)
-if [[ unameStr == 'Astral' ]]; then
+if [[ $unameStr == 'Astral' ]]; then
 	g++ -std=c++23 -O2 main.cpp -o main -lncurses -lcurses -ltinfow
 else
 	g++ -std=c++23 -O2 main.cpp -o main -lncurses -lcurses
